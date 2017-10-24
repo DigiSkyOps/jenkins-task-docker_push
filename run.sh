bc98538b07779999c069be848ca73a83d60a5779
Sean Ly <seanly@aliyun.com>
2017-10-24 18:01:27 +0800

    format plugin

--- run.sh
+++ run.sh
@@ -34,7 +34,8 @@ info() { printf "${white}➜ %s${reset}\n" "$@"
 }
 success() { printf "${green}✔ %s${reset}\n" "$@"
 }
-error() { printf "${red}✖ %s${reset}\n" "$@"
+error() { 
+  printf "${red}✖ %s${reset}\n" "$@"
   exit -1
 }
 warn() { printf "${tan}➜ %s${reset}\n" "$@"
@@ -57,7 +58,6 @@ type_exists() {
 # docker.push.registry
 # docker.push.username
 # docker.push.password
-# docker.push.email
 if [ -z "$ABS_DOCKER_PUSH_IMAGE" ]; then
   info 'Please build the image before pushing it'
   error 'A Docker image is required.'
@@ -82,6 +82,24 @@ type_exists() {
   return 1
 }
 
+docker_login() {
+
+  USERNAME="--username $ABS_DOCKER_PUSH_USERNAME"
+  PASSWORD="--password $ABS_DOCKER_PUSH_PASSWORD"
+
+  # Login to the registry
+  info 'login to the docker registry'
+  DOCKER_LOGIN="docker login $USERNAME $PASSWORD $REGISTRY"
+  debug `echo $DOCKER_LOGIN | tr "$ABS_DOCKER_PUSH_PASSWORD" '***********'`
+  docker login $USERNAME $PASSWORD $REGISTRY
+
+  if [[ $? -ne 0 ]]; then
+    error 'docker login errored';
+  else
+    success 'docker login succeed';
+  fi
+}
+
 docker_logout() {
   # Logout from the registry
   info 'logout to the docker registry'
@@ -96,43 +114,30 @@ docker_logout() {
   fi
 }
 
+push_image() {
+  # Push the docker image
+  info 'pushing docker image'
+
+  DOCKER_PUSH="docker push $ABS_DOCKER_PUSH_IMAGE"
+  debug "$DOCKER_PUSH"
+  docker push $ABS_DOCKER_PUSH_IMAGE
+
+  if [[ $? -ne 0 ]];then
+    docker_logout
+    error 'docker push errored';
+  else
+    success 'docker push succeed';
+  fi
+}
+
 # Check Docker is installed
 if ! type_exists 'docker'; then
   info 'Please use a box with docker installed'
   error 'Docker is not installed on this box.'
 fi
 
-set +e
-
-USERNAME="--username $ABS_DOCKER_PUSH_USERNAME"
-PASSWORD="--password $ABS_DOCKER_PUSH_PASSWORD"
-
-# Login to the registry
-info 'login to the docker registry'
-DOCKER_LOGIN="docker login $USERNAME $PASSWORD $REGISTRY"
-debug `echo $DOCKER_LOGIN | tr "$ABS_DOCKER_PUSH_PASSWORD" '***********'`
-docker login $USERNAME $PASSWORD $REGISTRY
-
-if [[ $? -ne 0 ]]; then
-  error 'docker login errored';
-else
-  success 'docker login succeed';
-fi
-
-# Push the docker image
-info 'pushing docker image'
-
-DOCKER_PUSH="docker push $ABS_DOCKER_PUSH_IMAGE"
-debug "$DOCKER_PUSH"
-docker push $ABS_DOCKER_PUSH_IMAGE
-
-if [[ $? -ne 0 ]];then
-  docker_logout
-  error 'docker push errored';
-else
-  success 'docker push succeed';
-fi
-
+docker_login
+push_image
 docker_logout
 
 set -e
